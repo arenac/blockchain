ab2a71262bb4ef06c248a28de6400ba86c5474e2
Nilo Neregato <neregato.nilo@gmail.com>
2020-08-26 22:40:43 +0200

    add adjust_difficult method

--- backend/blockchain/block.py
+++ backend/blockchain/block.py
@@ -1,6 +1,7 @@
 import time
 
 from backend.util.crypto_hash import crypto_hash
+from backend.config import MINE_RATE
 
 GENESIS_DATA = {
   'timestamp': 1,
@@ -24,6 +25,17 @@ class Block:
     self.difficulty = difficulty
     self.nonce = nonce
 
+  def __repr__(self):
+    return (
+      'Block('
+      f'timestamp: {self.timestamp}, '
+      f'last_hash: {self.last_hash}, '
+      f'hash: {self.hash}, '
+      f'data: {self.data}, '
+      f'difficulty: {self.difficulty}, '
+      f'nonce: {self.nonce})'
+    )
+
   @staticmethod
   def mine_block(last_block, data):
     """
@@ -32,13 +44,14 @@ class Block:
     """
     timestamp = time.time_ns()
     last_hash = last_block.hash
-    difficulty = last_block.difficulty
+    difficulty = Block.adjust_difficult(last_block, timestamp)
     nonce = 0
     hash = crypto_hash(timestamp, last_hash, data, difficulty, nonce)
 
     while hash[0:difficulty] != '0' * difficulty:
       nonce += 1
       timestamp = time.time_ns()
+      difficulty = Block.adjust_difficult(last_block, timestamp)
       hash = crypto_hash(timestamp, last_hash, data, difficulty, nonce)
 
     return Block(timestamp, last_hash, hash, data, difficulty, nonce)
@@ -50,16 +63,21 @@ class Block:
     """
     return Block(**GENESIS_DATA)
 
-  def __repr__(self):
-    return (
-      'Block('
-      f'timestamp: {self.timestamp}, '
-      f'last_hash: {self.last_hash}, '
-      f'hash: {self.hash}, '
-      f'data: {self.data}, '
-      f'difficulty: {self.difficulty}, '
-      f'nonce: {self.nonce})'
-    )
+  @staticmethod
+  def adjust_difficult(last_block, new_timestamp):
+    """
+    Calculate the adjusted difficultu according to the MINE_RATE.
+    Increase the difficulty for quickcly mine blocks.
+    Decrease the difficulty for quickly mine blocks.
+    """
+    if(new_timestamp - last_block.timestamp) < MINE_RATE:
+      return last_block.difficulty + 1
+
+    if(last_block.difficulty - 1) > 0:
+      return last_block.difficulty - 1
+
+    return 1
+
 
 
 def main():

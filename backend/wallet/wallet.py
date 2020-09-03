5f5935b71e4115230b80fbd0abcf3d00762621bb
Nilo Neregato <neregato.nilo@gmail.com>
2020-09-03 22:41:04 +0200

    validate the wallet

--- backend/wallet/wallet.py
+++ backend/wallet/wallet.py
@@ -5,6 +5,7 @@ from backend.config import STARTING_BALANCE
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import ec
 from cryptography.hazmat.primitives import hashes
+from cryptography.exceptions import InvalidSignature
 
 class Wallet:
   """
@@ -30,14 +31,34 @@ class Wallet:
       ec.ECDSA(hashes.SHA256())
     )
 
+  @staticmethod
+  def verify(public_key, data, signature):
+    """
+    VErify a signature based on the original publis key and data.
+    """
+    try:
+      public_key.verify(
+        signature,
+        json.dumps(data).encode('utf-8'),
+        ec.ECDSA(hashes.SHA256())
+      )
+      return True
+    except InvalidSignature:
+       return False
+
 def main():
   wallet = Wallet()
   print(f'wallet._dict__: {wallet.__dict__}')
 
   data = { 'trail': 'bike' }
   signature = wallet.sign(data)
-
   print(f'signature: {signature}')
+
+  should_be_valid = Wallet.verify(wallet.public_key, data, signature)
+  print(f'should_be_valid: {should_be_valid}')
+
+  should_be_invalid = Wallet.verify(Wallet().public_key, data, signature)
+  print(f'should_be_invalid: {should_be_invalid}')
 
 if __name__ == '__main__':
   main()

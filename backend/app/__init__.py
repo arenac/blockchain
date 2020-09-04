3243890403b1a1be5b6acde9110af42c9364283d
Nilo Neregato <neregato.nilo@gmail.com>
2020-09-04 23:20:55 +0200

    /wallet/transaction route

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -4,13 +4,16 @@ import random
 
 from decouple import config
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 from backend.blockchain.blockchain import Blockchain
 from backend.blockchain.pubsub import PubSub
+from backend.wallet.wallet import Wallet
+from backend.wallet.transaction import Transaction
 
 app = Flask(__name__)
 blockchain = Blockchain()
+wallet = Wallet()
 pubsub = PubSub(blockchain)
 
 @app.route('/')
@@ -31,6 +34,19 @@ def route_blockchain_mine():
   pubsub.broadcast_block(block)
 
   return jsonify(block.to_json())
+
+@app.route('/wallet/transaction', methods=['POST'])
+def route_wallet_transaction():
+  transaction_data = request.get_json()
+  transaction = Transaction(
+    wallet,
+    transaction_data['recipient'],
+    transaction_data['amount']
+  )
+
+  print(f'transaction.to_json(): {transaction.to_json()}')
+
+  return jsonify(transaction.to_json())
 
 PORT = config('PORT')
 url = config('API_URL')

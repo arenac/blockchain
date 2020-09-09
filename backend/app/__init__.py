70b98e4b9427da8b596f04d8e6e5cfa80fe21a5a
Nilo Neregato <neregato.nilo@gmail.com>
2020-09-09 23:16:02 +0200

    reward transaction method to reward the first miner who rewards successful a block ar first

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -28,7 +28,9 @@ def route_blockchain():
 
 @app.route('/blockchain/mine', methods=['GET'])
 def route_blockchain_mine():
-  blockchain.add_block(transaction_pool.transation_data())
+  transaction_data = transaction_pool.transation_data()
+  transaction_data.append(Transaction.reward_transaction(wallet).to_json())
+  blockchain.add_block(transaction_data)
   block = blockchain.chain[-1]
   pubsub.broadcast_block(block)
   transaction_pool.clear_blockchain_transactions(blockchain)

b6bb521acf63c8d21c89dadaa8cda9ce14650c1e
Nilo Neregato <neregato.nilo@gmail.com>
2020-10-09 23:42:27 +0200

    Submit a transaction

--- web/src/components/Blockchain/styles.ts
+++ web/src/components/Blockchain/styles.ts
@@ -1,9 +1,5 @@
 import styled from 'styled-components';
 
-import {
-  TablePagination as tablePagination
-} from '@material-ui/core';
-
 export const Container = styled.div`
   & {
     margin-top: 20px;
--- web/src/router/index.tsx
+++ web/src/router/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import ConductTransaction from '../components/ConductTransaction';
 
 import Home from '../pages/Home';
 
 const Router: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Home} />
+    <Route path="/transactions" component={ConductTransaction} />
   </Switch>
 )
 
